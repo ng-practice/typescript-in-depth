4a6088b24ccb04fc88d91b41a7f31b8816b6debc
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-10-17 13:51:00 +0200

    add exhaustiveness check

--- src/task-board/index.ts
+++ src/task-board/index.ts
@@ -29,13 +29,36 @@ function addTaskToList(
 }
 
 function getSortedDescByPriority(tasks: any[]): any[] {
-  return tasks.sort((a, b) => a.priority.localeCompare(b.priority));
+  return tasks.sort((a: { priority: string }, b: { priority: string }) =>
+    a.priority.localeCompare(b.priority)
+  );
   // return tasks.sort(function compareFn(a, b) {
   //   return b.priority - a.priority;
   // });
 }
 
+function assertNever(value: never): never {
+  throw new Error(`Expected never to be called but got ${value}`);
+}
+
+function dueDates(tasks: any[]) {
+  return tasks.map((task: { priority: TaskPriority }) => {
+    switch (task.priority) {
+      case TaskPriority.Low:
+        return 'Sometimes';
+      case TaskPriority.Medium:
+        return 'Today';
+      case TaskPriority.High:
+        return 'Now';
+      default:
+        assertNever(task.priority);
+    }
+  });
+}
+
 addTaskToList(id, title, text, TaskPriority.Low, isDone);
 addTaskToList(id, title, text, TaskPriority.High, isDone);
+addTaskToList(id, title, text, TaskPriority.Medium, isDone);
 
-console.log(getSortedDescByPriority(tasks));
+// console.log(getSortedDescByPriority(tasks));
+console.log(dueDates(tasks));
