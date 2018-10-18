713e2075ade3b7ac9cada7e049b4bfb5384d3ab9
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-10-18 09:54:37 +0200

    introduce barrel file for models

--- src/task-board/index.ts
+++ src/task-board/index.ts
@@ -1,5 +1,4 @@
-import { TaskList } from './models/task-list';
-import { TaskPriority } from './models/task-priority';
+import { TaskList, TaskPriority } from './models';
 
 const taskList = new TaskList();
 const id = '1234-234-23423-4324';
@@ -8,8 +7,20 @@ const text = 'text';
 const isDone = true;
 
 taskList.addTaskToList({ id, title, text, priority: TaskPriority.Low, isDone });
-taskList.addTaskToList({ id, title, text, priority: TaskPriority.High, isDone });
-taskList.addTaskToList({ id, title, text, priority: TaskPriority.Medium, isDone });
+taskList.addTaskToList({
+  id,
+  title,
+  text,
+  priority: TaskPriority.High,
+  isDone
+});
+taskList.addTaskToList({
+  id,
+  title,
+  text,
+  priority: TaskPriority.Medium,
+  isDone
+});
 
 console.log('Sorted:', taskList.getSortedDescByPriority());
 console.log('Due Dates:', taskList.dueDates());
